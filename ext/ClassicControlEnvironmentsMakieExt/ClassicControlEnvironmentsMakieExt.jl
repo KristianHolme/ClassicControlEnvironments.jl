e1b66f931775dd621921eb5691abd76d9aa7967a
Kristian Holme <holme.kristian@gmail.com>
2026-03-15 20:46:34 +0100

    use DrillInterface

--- ext/ClassicControlEnvironmentsMakieExt/ClassicControlEnvironmentsMakieExt.jl
+++ ext/ClassicControlEnvironmentsMakieExt/ClassicControlEnvironmentsMakieExt.jl
@@ -2,7 +2,7 @@ module ClassicControlEnvironmentsMakieExt
 
 using Makie
 using ClassicControlEnvironments
-using Drill
+using DrillInterface
 include("PendulumMakieExt.jl")
 include("MountainCarMakieExt.jl")
 include("CartPoleMakieExt.jl")
